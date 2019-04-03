c069a7c3a5178175ba6042b4da3e3b96ac4db6de
serchemach <serchemach@gmail.com>
2019-04-04 01:14:45 +0300

    New version

--- Project.py
+++ Project.py
@@ -1,65 +1,107 @@
 from tkinter import *
 from tkinter import filedialog as fd
+gridlenght = 25
+
+
+def draw(n, m):
+    root.geometry(str(gridlenght * n + 200) + 'x' + str(gridlenght * m + 100))  # window geometry
+    root.resizable(0, 0)
+
+    global w
+    w = Canvas(root, width=gridlenght * n, height=gridlenght * m)  # canvas for drawing and binds for mouse events
+    w.bind("<Button-1>", callbackl)
+    w.bind('<Button-3>', callbackr)
+    w.place(x=0, y=0)
+
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                color = 'blue'
+            else:
+                color = 'white'
+
+            w.create_rectangle(i * gridlenght, j * gridlenght, i * gridlenght + gridlenght, j * gridlenght + gridlenght,
+                               fill=color)  # grid
 
 
 def callbackl(event):
     global grid
-    w.create_rectangle((event.x // 50) * 50, (event.y // 50) * 50, (event.x // 50) * 50 + 50, (event.y // 50) * 50 + 50, fill='blue')
-    grid[(event.y // 50)][(event.x // 50)] = 1
+    w.create_rectangle((event.x // gridlenght) * gridlenght, (event.y // gridlenght) * gridlenght,
+                       (event.x // gridlenght) * gridlenght + gridlenght,
+                       (event.y // gridlenght) * gridlenght + gridlenght, fill='blue')
+    grid[(event.y // gridlenght)][(event.x // gridlenght)] = 1
     print('\n'.join(map(str, grid)))
     print("clicked at", event.x, event.y)
 
 
 def callbackr(event):
-    w.create_rectangle((event.x // 50) * 50, (event.y // 50) * 50, (event.x // 50) * 50 + 50, (event.y // 50) * 50 + 50, fill='white')
-    grid[(event.y // 50)][(event.x // 50)] = 0
+    w.create_rectangle((event.x // gridlenght) * gridlenght, (event.y // gridlenght) * gridlenght,
+                       (event.x // gridlenght) * gridlenght + gridlenght,
+                       (event.y // gridlenght) * gridlenght + gridlenght, fill='white')
+    grid[(event.y // gridlenght)][(event.x // gridlenght)] = 0
     print('\n'.join(map(str, grid)))
     print("clicked at", event.x, event.y)
 
 
 def filedialopen():
+    global grid, n, m
     file_name = fd.askopenfilename(filetypes=(("Level files", "*.lvl"), ("All files", "*.*")))
     inp = open(file_name)
-    s = inp.readline()
+    n, m = map(int, inp.readline().rstrip().split())
+
+    s = inp.readline().rstrip()
+    grid = []
     while s:
-        print(s.rstrip())
-        s = inp.readline()
+        grid.append(list(map(int, s.split())))
+        s = inp.readline().rstrip()
 
     inp.close()
+    draw(n, m)
 
 
 def filedialwrite():
-    global grid
+    global grid, n, m
     file_name = fd.asksaveasfilename(filetypes=(("Level files", "*.lvl"), ("All files", "*.*")))
     out = open(file_name, 'w')
-    print(10, 10, file=out)
-    for i in range(10):
-        for j in range(10):
+    print(n, m, file=out)
+    for i in range(n):
+        for j in range(m):
             print(grid[i][j], file=out, end=' ')
         print(file=out)
     out.close()
 
 
-grid = [[0 for i in range(10)] for j in range(10)]
-root = Tk()
-root.geometry('700x600')
-root.resizable(0, 0)
+def window():
+    global n, m
+    new.destroy()
+    file.destroy()
 
-w = Canvas(root, width=500, height=500)
-w.bind("<Button-1>", callbackl)
-w.bind('<Button-3>', callbackr)
-w.place(x=0, y=0)
-w.create_rectangle(2, 2, 400, 400)
+    draw(n, m)
 
-for i in range(10):
-    for j in range(10):
-        w.create_rectangle(i * 50, j * 50, i * 50 + 50, j * 50 + 50, fill='white')
+    menubar = Menu(root)  # menu for handling of files
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label='Open', command=filedialopen)
+    filemenu.add_command(label='Save', command=filedialwrite)
+    menubar.add_cascade(label='File', menu=filemenu)
 
-buttonopen = Button(text="Открыть", command=filedialopen)
-buttonopen.pack(side='right')
+    root.config(menu=menubar)
 
-buttonwrite = Button(text='Сохранить', command=filedialwrite)
-buttonwrite.pack(side='bottom')
 
-mainloop()
+def fileopen():
+    filedialopen()
+    window()
 
+
+if __name__ == '__main__':
+    n = 10
+    m = 10
+    grid = [[0 for i in range(10)] for j in range(10)]  # grid of blocks
+    root = Tk()
+
+    new = Button(text='New', command=window)
+    new.pack()
+
+    file = Button(text='Open', command=fileopen)
+    file.pack()
+
+    mainloop()
