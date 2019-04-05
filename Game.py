2c173def25ab497bc2c9321425b227c41f60ea00
serchemach <serchemach@gmail.com>
2019-04-05 11:49:30 +0300

    Score

--- Game.py
+++ Game.py
@@ -3,11 +3,14 @@ from tkinter import *
 from tkinter import filedialog as fd
 import Project
 rectangles = []
+types = [(0, 0, 255), (255, 0, 0), (255, 255, 0), (255, 165, 0), (0, 255, 0)]
+score = 0
 
 
 def main():
+    global score
     root.destroy()
-    x1 = 400
+    x1 = 500
     y1 = 300
     if rectangles:
         x1 = (m + 2) * 31
@@ -16,9 +19,7 @@ def main():
         for i in range(1, 4):
             for j in range(1, 12):
                 c = pygame.Rect(j * 31, i * 16, 30, 15)
-                rectangles.append(c)
-
-    ccolor = (0, 128, 255)
+                rectangles.append([c, 3])
 
     ball = pygame.Rect(200, y1 - 120, 20, 20)
     ballvelx = -3
@@ -26,11 +27,15 @@ def main():
     ballcolor = (255, 100, 0)
 
     pygame.init()
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = myfont.render('Score:', False, (255, 255, 255))
+
+
+
     screen = pygame.display.set_mode((x1, y1))
     done = False
 
     platform = pygame.Rect(175, y1 - 30, 50, 10)
-    platformcolor = (150, 90, 140)
 
     clock = pygame.time.Clock()
 
@@ -50,30 +55,34 @@ def main():
             platform.x = x1 - platform.width
         elif platform.x < 0:
             platform.x = 0
-        pygame.draw.rect(screen, platformcolor,  platform)
 
         for i in range(len(rectangles)):
             if rectangles[i] != 0:
                 coll = False
                 c = ball.move(ballvelx, 0)
-                if c.colliderect(rectangles[i]):
+                if c.colliderect(rectangles[i][0]):
                     ballvelx = -ballvelx
                     coll = True
 
                 c = ball.move(0, ballvely)
-                if c.colliderect(rectangles[i]):
+                if c.colliderect(rectangles[i][0]):
                     ballvely = -ballvely
                     coll = True
 
                 if coll:
-                    rectangles[i] = 0
-                    if rectangles == [0] * len(rectangles):
-                        quit()
-                    ball.x += ballvelx
-                    ball.y += ballvely
+                    score += rectangles[i][1] * 10
+                    if rectangles[i][1] > 1:
+                        rectangles[i][1] -= 1
+                    else:
+                        rectangles[i] = 0
+                        if rectangles == [0] * len(rectangles):
+                            quit()
+                        ball.x += ballvelx
+                        ball.y += ballvely
                 else:
-                    pygame.draw.rect(screen, ccolor, rectangles[i])
+                    pygame.draw.rect(screen, types[rectangles[i][1] - 1], rectangles[i][0])
 
+        platformcolor = (150, 90, 140)
         if ball.move(ballvelx, ballvely).colliderect(platform):
             coll = False
             c = ball.move(ballvelx, 0)
@@ -87,6 +96,7 @@ def main():
                 coll = True
 
             if coll:
+                platformcolor = (255, 255, 255)
                 ball.x += ballvelx
                 ball.y += ballvely
 
@@ -106,7 +116,13 @@ def main():
         if ball.y > y1 - ball.width - 5:
             quit()
 
+        pygame.draw.rect(screen, platformcolor,  platform)
         pygame.draw.rect(screen, ballcolor, ball)
+
+        screen.blit(textsurface, (x1 - 65, 0))
+        textsurface1 = myfont.render(str(score), False, (255, 255, 255))
+        screen.blit(textsurface1, (x1 - 65, 15))
+
 
         pygame.display.flip()
         clock.tick(60)
@@ -124,7 +140,7 @@ def openfile():
             if s[j] == '0':
                 rectangles.append(0)
             else:
-                rectangles.append(pygame.Rect(j * 31, i * 16, 30, 15))
+                rectangles.append([pygame.Rect(j * 31, i * 16, 30, 15), int(s[j])])
         s = inp.readline().rstrip().split()
 
     inp.close()
